359abe0092d1fd678a26fc9bb3249aee48f40e20
eduardo escobar <war_army_10@hotmail.com>
2020-10-06 20:36:37 -0500

    Adding RoomsController Functions and Views on Reservation and Room

--- app/Http/Controllers/RoomsController.php
+++ app/Http/Controllers/RoomsController.php
@@ -7,8 +7,19 @@ use Illuminate\Http\Request;
 class RoomsController extends Controller
 {
     //
-    public function checkAvailableRooms()
+    public function checkAvailableRooms($client_id, Request $request)
     {
+        $dateFrom = $request->input('dateFrom');
+        $dateTo = $request->input('dateTo');
+        $client = new Client();
+        $room = new Room();
+
+        $data = [];
+        $data['dateFrom'] = $dateFrom;
+        $data['dateTo'] = $dateTo;
+        $data['rooms'] = $room->getAvailableRooms($dateFrom, $dateTo);
+        $data['client'] = $client->find($client_id);
+        
         return view('rooms/checkAvailableRooms');
     }
 }
--- app/Room.php
+++ app/Room.php
@@ -3,8 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Room extends Model
 {
     //
+    public function getAvailableRooms($start_date, $end_date)
+    {
+        $available_rooms = DB::table('rooms as r')
+            ->select('r.id', 'r.name')
+        ->whereRaw("
+            r.id NOT IN(
+                select b.room_id from reservations b
+                where NOT(
+                    b.date_out < '{$start_date}' OR 
+                    b.date_in > '{$end_date}'   
+                )
+            )
+        ")->orderBy('r.id')
+        ->get();
+    }
 }
